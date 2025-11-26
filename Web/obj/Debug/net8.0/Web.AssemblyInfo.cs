e6a12ea0f907e01cfe08fab803490ed850de524e
LucioP7 <pianettilucio7@gmail.com>
2025-11-26 14:21:00 -0300

    Redirección tras login y actualización de recursos
    Se añadió una redirección en `Login.razor` para que, tras la autenticación exitosa, el usuario sea redirigido a la página principal (`"/"`) utilizando `NavigationManager.NavigateTo("/")`.
    
    Se actualizaron los archivos binarios (`Web.dll`, `Web.pdb`, `Web.wasm`, y sus versiones comprimidas) reflejando los cambios en el código fuente.
    
    Se realizaron ajustes en los archivos de configuración de recursos estáticos (`staticwebassets.build.json`, `staticwebassets.development.json`, `staticwebassets.build.json.cache`) para actualizar rutas, índices de contenido y valores hash, asegurando la integridad y consistencia de los recursos.
    
    Se actualizó el archivo `blazor.boot.json` con nuevos valores hash para los recursos binarios y otros archivos relacionados. Además, se modificó `Web.AssemblyInfo.cs` para reflejar un nuevo identificador de versión (`1.0.0+8b6fa58d52aaa1d978944a7680508ec8cca49fe1`).
    
    Estos cambios garantizan la correcta gestión de recursos estáticos, la integridad de los binarios y la incorporación de nuevas funcionalidades en el flujo de autenticación.

--- Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9626711d0c5cd570fe15fb72f4304c13d0916244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b6fa58d52aaa1d978944a7680508ec8cca49fe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
